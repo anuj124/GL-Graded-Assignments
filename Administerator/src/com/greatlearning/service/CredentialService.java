13256d2fb0c2dd09badbc09e5a483eb48368ee17
Anuj <Anuj@DESKTOP-H9UMQ1R>
2022-06-19 22:37:03 +0530

    updated runtime exeption of index of bound

---
+++ Administerator/src/com/greatlearning/service/CredentialService.java
@@ -0,0 +1,38 @@
+package com.greatlearning.service;
+
+import java.util.Random;
+
+import com.greatlearning.model.Employee;
+
+public class CredentialService {
+	String capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	String smallLetter = "abcdefghijklmnopqrstuvwxyz";
+	String number = "0123456789";
+	String specialCharacter = "!@#$%^&*()_+-={}|[];:?|/<>,.";
+	char[] password = new char[8];
+	String values = capitalLetter + smallLetter + number + specialCharacter;
+	String email;
+
+	public char[] generatePassword() {
+		Random random = new Random();
+		for (int i = 0; i < 8; i++) {
+			password[i] = values.charAt(random.nextInt(values.length()));
+		}
+		return password;
+	}
+
+	public String generateEmail(String firstName, String lastName, String department) {
+		return email = firstName + lastName + "@" + department + "abc.com";
+
+	}
+
+	public void showCredentials(Employee employee, String email, char[] generatedPassword, String department) {
+		System.out.println("Dear " + employee.getFirstName() + " " + employee.getLastName()
+				+ " your generated credentials as follows");
+		System.out.println("Department : " +department);
+		System.out.println("Email : " + email);
+		System.out.println("Password : " + generatedPassword.toString());
+
+	}
+
+}
