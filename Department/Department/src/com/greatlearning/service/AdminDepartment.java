d80769d1b7436f6fc0bf06094afd939f787fbbd0
Anuj Sharma <53485174+anuj124@users.noreply.github.com>
2022-06-23 11:15:06 +0530

    uploaded code with better Indentation

---
+++ Department/Department/src/com/greatlearning/service/AdminDepartment.java
@@ -0,0 +1,17 @@
+package com.greatlearning.service;
+
+public class AdminDepartment extends SuperDepartment {
+
+	public String departmentName() {
+		return ("Admin Department");
+	}
+
+	public String getTodaysWork() {
+		return ("Complete Your Documents Submission");
+	}
+
+	public String getWorkDeadline() {
+		return ("Complete by EOD");
+	}
+
+}
