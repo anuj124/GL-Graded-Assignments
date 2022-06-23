5af87eafe743d853153046dde0de8ebff338840e
Anuj Sharma <53485174+anuj124@users.noreply.github.com>
2022-06-23 11:00:44 +0530

    Graded Coded Assignment 1

---
+++ Department/src/com/greatlearning/service/TechDepartment.java
@@ -0,0 +1,22 @@
+package com.greatlearning.service;
+
+public class TechDepartment extends SuperDepartment {
+	
+	public String departmentName() {
+		return("Tech Department");
+	}
+	
+	public String getTodaysWork() {
+		return("Complete code of module 1");
+	}
+	
+	public String getWorkDeadline() {
+		return("Complete by EOD");
+				
+	}
+	
+	public String getTechStackInformation() {
+		return("core java");
+	}
+
+}
