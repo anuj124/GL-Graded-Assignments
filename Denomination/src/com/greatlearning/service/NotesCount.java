deae0ba768b74257c177ef80438ad1d74dab7032
Anuj Sharma <53485174+anuj124@users.noreply.github.com>
2022-07-10 19:55:56 +0530

    Lab 2 Question 2

---
+++ Denomination/src/com/greatlearning/service/NotesCount.java
@@ -0,0 +1,34 @@
+package com.greatlearning.service;
+
+public class NotesCount {
+
+	public void notesCountImplementation(int[] notes, int amount) {
+		int[] notesCount = new int[notes.length];
+
+		try {
+			for (int i = 0; i < notes.length; i++) {
+				if (amount > notes[i]) {
+					notesCount[i] = amount / notes[i];
+					amount = amount - notesCount[i] * notes[i];
+				}
+			}
+			if (amount > 0) {
+				System.out.println("This amouunt can not be paid by the available denominations of notes");
+				System.out.println("Please enter the amount which can be paid by following denominations");
+				for (int i = 0; i < notes.length; i++) {
+					System.out.print(notes[i] + ",");
+				}
+			} else {
+				System.out.println("Your Amount will be paid in following denominations: ");
+				for (int i = 0; i < notes.length; i++) {
+					System.out.println(notes[i] + "-->" + notesCount[i]);
+				}
+			}
+		} catch (ArithmeticException e) {
+			System.out.println(e + "notes of denomination in 0 is invalid");
+		}
+		{
+
+		}
+	}
+}
